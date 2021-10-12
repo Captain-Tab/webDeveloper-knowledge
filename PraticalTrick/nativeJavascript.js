6d8c61d0f4c4ceda544b171172bd5b82f30fa0f3
captain-tab <d491405879@gmail.com>
2021-10-12 23:25:26 +0800

    feat: update nativescript

--- PraticalTrick/nativeJavascript.js
+++ PraticalTrick/nativeJavascript.js
@@ -284,6 +284,27 @@ const obj = {
 const result = Object.myValues(obj)
 console.log('result', result)
 
+/*****************Native Create*********************/
+Object.prototype.myCreate = function (proto, propertyObject = undefined) {
+    if (typeof proto !== 'object' && typeof proto !== 'function') {
+        throw new TypeError('Object prototype may only be an Object or null.')
+        if (propertyObject == null) {
+            new TypeError('Cannot convert undefined or null to object')
+        }
+        function F() { }
+        F.prototype = proto
+        const obj = new F()
+        if (propertyObject != undefined) {
+            Object.defineProperties(obj, propertyObject)
+        }
+        if (proto === null) {
+            // 创建一个没有原型对象的对象，Object.create(null)
+            obj.__proto__ = null
+        }
+        return obj
+    }
+}
+
 /*****************Native InstanceOf*********************/
 function myInstanceOf(father, child) {
     const fp = father.prototype
